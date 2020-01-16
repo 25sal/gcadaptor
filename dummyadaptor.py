363637208b055533a925e7d28494cf2a3493bbd9
salvatore <salvatore@parsec2.unina2.it>
2020-01-16 14:24:27 +0100

    updated on parsec2

--- dummyadaptor.py
+++ dummyadaptor.py
@@ -17,22 +17,27 @@ import json
 import queue
 
 
-base_url = "http://localhost:5280/files"
+base_url = "http://parsec2.unicampania.it:5280/files"
 
 
 def fill_queue(the_queue):
-    the_queue.put_nowait({"time": 1475931040,
+    the_queue.put_nowait({"sim_id":'"demo"',
+			   "time": 1475931040,
                           "message": '{"subject": "SIMULATION","status": "started"}'})
-    the_queue.put_nowait({"time": 1475931047,
+    the_queue.put_nowait({"sim_id":'"demo"',
+			  "time": 1475931047,
                           "message": '{"subject": "CREATE_PRODUCER", "type": "PV", "id": "[0]:[1]", '
-                                     '"profile":"' + base_url + '/producer.csv"}'})
-    the_queue.put_nowait({"time": 1475932000,
+                                     '"profile":"' + base_url + '/demo/input/producer.csv"}'})
+    the_queue.put_nowait({"sim_id":'"demo"',
+			  "time": 1475932000,
                           "message": '{"subject": "LOAD", "id": "[0]:[1]:[1]", "sequence": 1, "est": 1475932000, '
-                                     '"lst": 1475939000,  "profile": "' + base_url + '/0_run_1.csv"}'})
-    the_queue.put_nowait({"time": 1475935000,
-                          "message": '{"subject": "ATTACH_HC_DEVICE", "id": "[0]:[2]:[1]", '
-                                     '"profile": "' + base_url + '/0_2_1.csv"}'})
-    the_queue.put_nowait({"time": 1475991040,
+                                     '"lft": 1475948000,  "profile": "' + base_url + '/demo/input/0_run_1.csv", "demo":"'+base_url+'/gcharge/index.html"}'})
+    the_queue.put_nowait({"sim_id":'"demo"',
+			  "time": 1475935000,
+                          "message": '{"subject": "HC_LOAD", "id": "[0]:[2]:[1]", "sequence":1,'
+                                     '"profile": "' + base_url + '/demo/input/0_2_1.csv","demo":"'+base_url+'/gcharge/indexhc.html"}'})
+    the_queue.put_nowait({"sim_id":'"demo"',
+			  "time": 1475991040,
                           "message": '{"subject": "SIMULATION","status": "stopped"}'})
 
 
@@ -43,7 +48,7 @@ class WebAgent(agent.Agent):
 
     def setup(self):
         fill_queue(self.dispatched)
-        self.web.start(templates_path="examples")
+        self.web.start(hostname="parsec2.unicampania.it", templates_path="examples")
         self.traces.reset()
 
     async def get_message(self, request):
@@ -51,11 +56,11 @@ class WebAgent(agent.Agent):
             fill_queue(self.dispatched)
         nextmsg = self.dispatched.get_nowait()
         self.simulated_time = nextmsg["time"]
-        return {"time": nextmsg["time"], "message": nextmsg["message"]}
+        return {"sim_id": nextmsg["sim_id"], "time": nextmsg["time"], "message": nextmsg["message"]}
 
     async def get_time(self, request):
 
-        return {"time": self.simulated_time, "message": '{"subject": "TIME", "time": ' + str(self.simulated_time) + '}'}
+        return {"sim_id": '"demo"', "time": self.simulated_time, "message": '{"subject": "TIME", "time": ' + str(self.simulated_time) + '}'}
 
     async def my_post_controller(self, request):
         form = await request.post()
@@ -68,7 +73,7 @@ class WebAgent(agent.Agent):
 def run(jid, pwd, port):
     a = WebAgent(jid, pwd)
     a.web.port = port
-
+    a.web.hostname="parsec2.unicampania.it"
     a.web.add_get("/getmessage", a.get_message, "message.html")
     a.web.add_get("/gettime", a.get_time, "message.html")
     a.web.add_post("/postmessage",a.my_post_controller, None)
---
+++ runadaptor.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3.7 dummyadaptor.py --jid adaptor@localhost --pwd secret
